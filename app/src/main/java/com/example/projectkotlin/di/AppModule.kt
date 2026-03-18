20d9c7cb01cf8329660d746e6c4cf4a91aac13c8
Vadim Denisov <vadimdenisovpsvu2@gmail.com>
2026-03-18 08:53:13 +0000

    Provisional estructura del proyecto

---
+++ app/src/main/java/com/example/projectkotlin/di/AppModule.kt
@@ -0,0 +1,24 @@
+package com.example.projectkotlin.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://pokeapi.co/api/v2/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
